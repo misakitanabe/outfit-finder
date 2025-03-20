549207c2683398ced593f455e288d0e5d26b41e1
misakitanabe <misaki.a.tanabe@gmail.com>
2025-03-19 21:18:26 -0700

    Fixed CSS for upload page

--- project-frontend/src/pages/Upload.tsx
+++ project-frontend/src/pages/Upload.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useActionState } from "react";
 import { IoHeart } from "react-icons/io5";
 import { ToastContainer, toast } from "react-toastify";
 import BounceLoader from "react-spinners/ClipLoader";
@@ -6,7 +6,6 @@ import "react-toastify/dist/ReactToastify.css";
 import Header from "../components/Header"
 import UploadedImage from "../components/UploadedImage";
 import Dropdown from "../components/Dropdown";
-import { ImageUploadForm } from "../components/ImageUploadForm";
 import './styles/Pages.css'
 
 interface UploadProps {
@@ -16,10 +15,52 @@ interface UploadProps {
 }
 
 function Upload(props : UploadProps) {
-    const [image, setImage] = useState<string | null>(null);
-    const [category, setCategory] = useState("");
-    const [color, setColor] = useState("");
+    const [category, setCategory] = useState("tops");
+    const [color, setColor] = useState("red");
     const [isFavorite, setIsFavorite] = useState(false);
+    const [dataUrl, setDataUrl] = useState("");
+    const [result, submitAction, isPending] = useActionState(
+        async (previousState: any, formData: FormData) => {
+            const image = formData.get("image");
+            const name = formData.get("name");
+            if (!name || !image) {
+                return {
+                    type: "error",
+                    message: `Please input both name and image`,
+                };
+            }
+            formData.append("category", category); 
+            formData.append("color", color);
+
+            try {
+                const response = await fetch("/api/images", {
+                    method: "POST",
+                    body: formData,
+                    headers: {
+                        "Authorization": `Bearer ${props.authToken}`,
+                    }
+                });
+                if (!response.ok) {
+                    // Handle HTTP 400 bad upload, HTTP 401 Unauthorized, etc...
+                    return {
+                        type: "error",
+                        message: `Client error`,
+                    };
+                }
+                return {
+                    type: "success",
+                    message: `Successfully uploaded image!`,
+                };
+            } catch (error) { // Network error
+                console.error(error);
+                return {
+                    type: "error",
+                    message: `Something went wrong`,
+                };
+            }
+        },
+        null
+    );
 
     const categories = [
         { value: "tops", label: "Tops" },
@@ -49,15 +90,6 @@ function Upload(props : UploadProps) {
         { value: "silver", label: "Silver" }
     ];
 
-    const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (e.target.files) {
-            const file = e.target.files[0]; 
-            if (file) {
-                setImage(URL.createObjectURL(file)); 
-            }
-        }
-    };
-
     const handleHeartClick = () => {
         setIsFavorite(!isFavorite);
 
@@ -68,11 +100,45 @@ function Upload(props : UploadProps) {
         });
     }
 
-    const handleSaveClick = () => {
-        toast.success("Succesfully saved", {
-            position: "top-right",
-            autoClose: 1000, 
-            hideProgressBar: true,
+    // const handleSaveClick = async () => {
+    //     while (!result) {
+    //         await new Promise(resolve => setTimeout(resolve, 100));
+    //     }
+    //     if (result.type === "success") {
+    //         toast.success(result.message, {
+    //             position: "top-right",
+    //             autoClose: 1000, 
+    //             hideProgressBar: true,
+    //         }) 
+    //     } else {
+    //         toast.error(result.message, {
+    //             position: "top-right",
+    //             autoClose: 1000, 
+    //             hideProgressBar: true,
+    //         }) 
+    //     }
+    // }
+
+    async function handleImageUpload(e: React.ChangeEvent<HTMLInputElement>) {
+        if (e.target.files) {
+            const fileObj = e.target.files[0];
+            try {
+                const url = await readAsDataURL(fileObj);
+                if (typeof url === "string") {
+                    setDataUrl(url);
+                }
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+
+    function readAsDataURL(file: File): Promise<string | ArrayBuffer | null> {
+        return new Promise((resolve, reject) => {
+            const fr = new FileReader();
+            fr.onload = () => resolve(fr.result);
+            fr.onerror = (err) => reject(err);
+            fr.readAsDataURL(file);
         });
     }
 
@@ -86,37 +152,66 @@ function Upload(props : UploadProps) {
                 {isFavorite ? <IoHeart className="heart" style={{ color: 'pink' }} /> : <IoHeart className="heart"/>}
             </button>
 
-            <h3><ImageUploadForm authToken={props.authToken} category={category} color={color} /></h3>
+            <form action={submitAction} className="uploading-form">
+                <div className="uploading-input">
+                    <label>
+                        Choose Item: 
+                        <input
+                            name="image"
+                            type="file"
+                            accept=".png,.jpg,.jpeg"
+                            onChange={handleImageUpload}
+                        />
+                    </label>
+                </div>
+                
+                <div className="name-input-container">
+                    <label>
+                        <span>Image title: </span>
+                        <input name="name" className="item-name-input" />
+                    </label>
+                </div>
 
+                <div className="preview-image"> {/* Preview img element */}
+                    {isPending && <BounceLoader
+                        color='var(--color-link)'
+                        loading={isPending}
+                        cssOverride={{flexShrink: 0, position: 'relative', top: '40%'}}
+                        size={50}
+                        aria-label="Loading Spinner"
+                    />}
+                    <img style={{maxWidth: "20em", position: "relative", top: "3em", left: "5em"}} src={dataUrl} alt="" />
+                </div>
 
-            {/* name input */}
-            {/* <label className="item-name-input">
-                Name Item
-                <input onChange={props.onChange} value={props.itemName} placeholder="Name Item" className="item-name-input"/>
-            </label> */}
+                {/* dropdown options */}
+                <div className="row-container">
+                    <Dropdown label='Category' options={categories} handleDropdownChange={(e) => {setCategory(e.target.value)}} />
+                    <Dropdown label='Color' options={colors} handleDropdownChange={(e) => {setColor(e.target.value)}}/>
+                </div>
+
+                {result && (
+                    result.type=== "success" 
+                    ?
+                    toast.success("Succesfully saved", {
+                        position: "top-right",
+                        autoClose: 1000, 
+                        hideProgressBar: true,
+                    }) 
+                    :
+                    toast.error(result.message, {
+                        position: "top-right",
+                        autoClose: 1000, 
+                        hideProgressBar: true,
+                    }) 
+                )}
+
+                {/* save button */}
+                <button disabled={isPending} className="save-button" 
+                // onClick={handleSaveClick}
+                >Save</button>
+
+            </form>
             
-            {/* image uploading box */}
-            {/* <input className="uploading-input" type="file" accept="image/*" onChange={handleImageUpload} />
-            
-            <div className="image-container">
-                {!image && <BounceLoader
-                    color='var(--color-link)'
-                    loading={!image}
-                    cssOverride={{flexShrink: 0, position: 'relative', top: '40%'}}
-                    size={50}
-                    aria-label="Loading Spinner"
-                />}
-                {image && <UploadedImage src={image} />}
-            </div> */}
-
-            {/* dropdown options */}
-            <div className="row-container">
-                <Dropdown label='Category' options={categories} handleDropdownChange={(e) => {setCategory(e.target.value)}} />
-                <Dropdown label='Color' options={colors} handleDropdownChange={(e) => {setColor(e.target.value)}}/>
-            </div>
-
-            {/* save button */}
-            <button className="save-button" onClick={handleSaveClick}>Save</button>
         </div>
     );
 }
